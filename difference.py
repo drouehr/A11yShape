e73b3c8557a62443b67a130303c6722928da18b4
acmyu <amycmyu@gmail.com>
2024-03-08 14:01:31 -0500

    add code update description to backend

--- difference.py
+++ difference.py
@@ -18,6 +18,8 @@ api_key = os.getenv("OPENAI_KEY")
 
 rep_key = os.getenv("REPLICATE_API_TOKEN")
 
+views_folder = 'temp'
+
 
 # Function to encode the image
 def encode_image(image_path):
@@ -30,14 +32,51 @@ def encode_image(image_path):
     return base64.b64encode(im_bytes).decode('utf-8')
 
 
-def desc_gpt4():
+def get_difference(code, prev):
+    
+    cl = code.split('module')
+    pl = prev.split('module')
+    
+    diff = []
+    
+    if len(cl) == len(pl):
+        for i in range(0, len(cl)):
+            if cl[i] != pl[i]:
+                #print('module'+pl[i])
+                diff.append({'bef': 'module'+pl[i], 'aft': 'module'+cl[i]})
+    if len(diff) == 0:
+        return 'no difference'
+    
+
+    file = 'before.scad'
+    f = open(join(views_folder, file), "w")
+    f.write(prev)
+    f.close()
+    fp = join(views_folder, file)
+    
+    outfile1 = fp.replace('.scad', '.png')
+    cmd = 'openscad -o '+outfile1+' -q --camera 0,0,0,0,0,0 --viewall --autocenter --imgsize=2048,2048 '+fp
+    os.system(cmd)
+    
+    file = 'after.scad'
+    f = open(join(views_folder, file), "w")
+    f.write(code)
+    f.close()
+    fp = join(views_folder, file)
+    
+    outfile2 = fp.replace('.scad', '.png')
+    cmd = 'openscad -o '+outfile2+' -q --camera 0,0,0,0,0,0 --viewall --autocenter --imgsize=2048,2048 '+fp
+    os.system(cmd)
+    
+    
+
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
     }
     
-    base64_image = encode_image('temp/d1.png')
-    base64_image2 = encode_image('temp/d2.png')
+    base64_image = encode_image(outfile1)
+    base64_image2 = encode_image(outfile2)
 
     payload = {
         "model": "gpt-4-vision-preview",
@@ -56,14 +95,11 @@ def desc_gpt4():
                 "text": """
                 Before:
 
-                bacteriophage();
+                """+diff[0]['bef']+"""
 
                 After:
 
-                difference() {
-                    bacteriophage();
-                    translate([0,0,-5]) cube([100,100,10], center=true);
-                }
+                """+diff[0]['aft']+"""
                 """
               },
               {
@@ -92,7 +128,186 @@ def desc_gpt4():
 
 
 
-print(desc_gpt4())
+
+
+prev = """
+    // Bacteriophage by Erik Wrenholt 2017-02-12
+// License: Creative Commons - Attribution
+
+$fn = 12;
+
+leg_count = 6;
+leg_width = 1.75;
+
+printable_phage();
+
+module printable_phage() {
+    // chop off the bottom of the legs so they are flat on the bottom.
+    bacteriophage();
+}
+
+module bacteriophage() {
+    body();
+    for(i=[0:leg_count]) {
+        rotate((360 / leg_count) * i, [0,0,1])
+            leg();
+    }
+ }
+
+module body() {
+    
+    // Icosahedral head
+    translate([0,0,30]) 
+    scale([1,1,1.3])
+        rotate(30, [0,1,0]) 
+                icosahedron(5);
+    
+    // Base-Plate
+    translate([0,0,1.5])
+        scale([1,1,0.4])
+            rotate(30, [0,0,1])
+                sphere(6, $fn=12);
+
+    // Helical Sheath
+    for(i=[2:10]) {
+        translate([0,0,i*2])
+            scale([1,1,0.5])
+                sphere(4);
+    }
+
+}
+
+module leg() {
+    union() {
+        hull() {
+            translate([2,0,0]) sphere(leg_width);
+            translate([15,0,12]) sphere(leg_width);
+        }
+        hull() {
+            translate([15,0,12]) sphere(leg_width);
+            translate([25,0,-2]) sphere(leg_width);
+        }
+    }
+}
+
+
+// https://www.thingiverse.com/thing:1343285
+
+/*****************************************************************
+* Icosahedron   By Adam Anderson
+* 
+* This module allows for the creation of an icosahedron scaled up 
+* by a factor determined by the module parameter. 
+* The coordinates for the vertices were taken from
+* http://www.sacred-geometry.es/?q=en/content/phi-sacred-solids
+*************************************************************/
+
+module icosahedron(a = 2) {
+    phi = a * ((1 + sqrt(5)) / 2);
+    polyhedron(
+        points = [
+            [0,-phi, a], [0, phi, a], [0, phi, -a], [0, -phi, -a], [a, 0, phi], [-a, 0, phi], [-a, 0, -phi], 
+            [a, 0, -phi], [phi, a, 0], [-phi, a, 0], [-phi, -a, 0], [phi, -a, 0]    
+        ],
+        faces = [
+            [0,5,4], [0,4,11], [11,4,8], [11,8,7], [4,5,1], [4,1,8], [8,1,2], [8,2,7], [1,5,9], [1,9,2], [2,9,6], [2,6,7], [9,5,10], [9,10,6], [6,10,3], [6,3,7], [10,5,0], [10,0,3], [3,0,11], [3,11,7]
+        ]
+    
+    );
+}
+    """
+    
+code = """
+    // Bacteriophage by Erik Wrenholt 2017-02-12
+// License: Creative Commons - Attribution
+
+$fn = 12;
+
+leg_count = 6;
+leg_width = 1.75;
+
+printable_phage();
+
+module printable_phage() {
+    // chop off the bottom of the legs so they are flat on the bottom.
+    difference() {
+        bacteriophage();
+        translate([0,0,-5]) cube([100,100,10], center=true);
+    }
+}
+
+module bacteriophage() {
+    body();
+    for(i=[0:leg_count]) {
+        rotate((360 / leg_count) * i, [0,0,1])
+            leg();
+    }
+ }
+
+module body() {
+    
+    // Icosahedral head
+    translate([0,0,30]) 
+    scale([1,1,1.3])
+        rotate(30, [0,1,0]) 
+                icosahedron(5);
+    
+    // Base-Plate
+    translate([0,0,1.5])
+        scale([1,1,0.4])
+            rotate(30, [0,0,1])
+                sphere(6, $fn=12);
+
+    // Helical Sheath
+    for(i=[2:10]) {
+        translate([0,0,i*2])
+            scale([1,1,0.5])
+                sphere(4);
+    }
+
+}
+
+module leg() {
+    union() {
+        hull() {
+            translate([2,0,0]) sphere(leg_width);
+            translate([15,0,12]) sphere(leg_width);
+        }
+        hull() {
+            translate([15,0,12]) sphere(leg_width);
+            translate([25,0,-2]) sphere(leg_width);
+        }
+    }
+}
+
+
+// https://www.thingiverse.com/thing:1343285
+
+/*****************************************************************
+* Icosahedron   By Adam Anderson
+* 
+* This module allows for the creation of an icosahedron scaled up 
+* by a factor determined by the module parameter. 
+* The coordinates for the vertices were taken from
+* http://www.sacred-geometry.es/?q=en/content/phi-sacred-solids
+*************************************************************/
+
+module icosahedron(a = 2) {
+    phi = a * ((1 + sqrt(5)) / 2);
+    polyhedron(
+        points = [
+            [0,-phi, a], [0, phi, a], [0, phi, -a], [0, -phi, -a], [a, 0, phi], [-a, 0, phi], [-a, 0, -phi], 
+            [a, 0, -phi], [phi, a, 0], [-phi, a, 0], [-phi, -a, 0], [phi, -a, 0]    
+        ],
+        faces = [
+            [0,5,4], [0,4,11], [11,4,8], [11,8,7], [4,5,1], [4,1,8], [8,1,2], [8,2,7], [1,5,9], [1,9,2], [2,9,6], [2,6,7], [9,5,10], [9,10,6], [6,10,3], [6,3,7], [10,5,0], [10,0,3], [3,0,11], [3,11,7]
+        ]
+    
+    );
+}
+    """
+
+#print(get_difference(code, prev))
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # https://stackoverflow.com/questions/77284901/upload-an-image-to-chat-gpt-using-the-api
 
+from difference import get_difference
+
 import base64
 import requests
 from dotenv import load_dotenv
@@ -107,6 +109,12 @@ def get_concat_h(im1, im2):
 
 def run(req, model):
     code = req['code']
+    if 'prev' in req:
+        d = get_difference(code, prev)
+        response = {'description': d}
+        return response
+    
+    
     file = 'model.scad'
     f = open(join(views_folder, file), "w")
     f.write(code)
@@ -222,6 +230,96 @@ def run(req, model):
     return response
 
 
+
+
+prev = """
+    // Bacteriophage by Erik Wrenholt 2017-02-12
+// License: Creative Commons - Attribution
+
+$fn = 12;
+
+leg_count = 6;
+leg_width = 1.75;
+
+printable_phage();
+
+module printable_phage() {
+    // chop off the bottom of the legs so they are flat on the bottom.
+    bacteriophage();
+}
+
+module bacteriophage() {
+    body();
+    for(i=[0:leg_count]) {
+        rotate((360 / leg_count) * i, [0,0,1])
+            leg();
+    }
+ }
+
+module body() {
+    
+    // Icosahedral head
+    translate([0,0,30]) 
+    scale([1,1,1.3])
+        rotate(30, [0,1,0]) 
+                icosahedron(5);
+    
+    // Base-Plate
+    translate([0,0,1.5])
+        scale([1,1,0.4])
+            rotate(30, [0,0,1])
+                sphere(6, $fn=12);
+
+    // Helical Sheath
+    for(i=[2:10]) {
+        translate([0,0,i*2])
+            scale([1,1,0.5])
+                sphere(4);
+    }
+
+}
+
+module leg() {
+    union() {
+        hull() {
+            translate([2,0,0]) sphere(leg_width);
+            translate([15,0,12]) sphere(leg_width);
+        }
+        hull() {
+            translate([15,0,12]) sphere(leg_width);
+            translate([25,0,-2]) sphere(leg_width);
+        }
+    }
+}
+
+
+// https://www.thingiverse.com/thing:1343285
+
+/*****************************************************************
+* Icosahedron   By Adam Anderson
+* 
+* This module allows for the creation of an icosahedron scaled up 
+* by a factor determined by the module parameter. 
+* The coordinates for the vertices were taken from
+* http://www.sacred-geometry.es/?q=en/content/phi-sacred-solids
+*************************************************************/
+
+module icosahedron(a = 2) {
+    phi = a * ((1 + sqrt(5)) / 2);
+    polyhedron(
+        points = [
+            [0,-phi, a], [0, phi, a], [0, phi, -a], [0, -phi, -a], [a, 0, phi], [-a, 0, phi], [-a, 0, -phi], 
+            [a, 0, -phi], [phi, a, 0], [-phi, a, 0], [-phi, -a, 0], [phi, -a, 0]    
+        ],
+        faces = [
+            [0,5,4], [0,4,11], [11,4,8], [11,8,7], [4,5,1], [4,1,8], [8,1,2], [8,2,7], [1,5,9], [1,9,2], [2,9,6], [2,6,7], [9,5,10], [9,10,6], [6,10,3], [6,3,7], [10,5,0], [10,0,3], [3,0,11], [3,11,7]
+        ]
+    
+    );
+}
+    """
+
+
 testing = """
     // Bacteriophage by Erik Wrenholt 2017-02-12
 // License: Creative Commons - Attribution
@@ -312,5 +410,5 @@ module icosahedron(a = 2) {
 }
     """
 
-#req = {'code': testing}
-#run(req, 'gpt4')
+req = {'code': testing, 'prev': prev}
+print(run(req, 'gpt4'))
